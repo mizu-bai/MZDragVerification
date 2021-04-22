5cc1f87878064d66274e54262a639e6c4195d33f
mizu-bai <shiragawa4519@outlook.com>
2021-04-22 11:26:57 +0800

    Fixed bugs

--- MZDragVerification/MZVerificationView.m
+++ MZDragVerification/MZVerificationView.m
@@ -115,7 +115,8 @@
         [self.buttonBullet setHighlighted:NO];
         dispatch_after(dispatch_time(DISPATCH_TIME_NOW, 1 * NSEC_PER_SEC), dispatch_get_main_queue(), ^{
             self.buttonBullet.transform = CGAffineTransformIdentity;
-            [self awakeFromNib];
+            // if you want to refresh automatically, awake this view
+            // [self awakeFromNib];
         });
     }
     if (self.verify) {
